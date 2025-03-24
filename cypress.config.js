f908fa2a00fc048e0b8ce6824e133a11c0473cfc
Owais Gondal <owaisgondal@live.com>
2025-03-24 14:08:32 +0500

    modify cypress.yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "opvmkb",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://automationexercise.com/',
+    baseUrl: "https://automationexercise.com/",
+    specPattern: "cypress/e2e/tests/*",
   },
+
 });
